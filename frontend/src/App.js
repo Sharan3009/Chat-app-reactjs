3d769635b60e502b63c96f9ed02f777e7b0690e9
Sharandeep Singh <>
2020-03-21 21:41:54 +0530

    using react component as a class

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,51 @@
-import React,{useState} from 'react';
+import React from 'react';
 import {Button, Modal} from 'react-bootstrap';
 import './App.css';
 
-function App() {
-  const [show, setShow] = useState(false);
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      show:false
+    }
+  }
+  
+  setShow(bool){
+    this.setState({show:bool});
+  }
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
+  handleClose(){
+    this.setShow(false);
+  }
+
+  handleShow(){
+    this.setShow(true);
+  }
+
+  render(){
+    return(
+      <>
+      <Button variant="primary" onClick={this.handleShow.bind(this)}>
+        Open Modal
       </Button>
 
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={this.state.show} onHide={this.handleClose.bind(this)}>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
         <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary" onClick={this.handleClose.bind(this)}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={this.handleClose.bind(this)}>
             Save Changes
           </Button>
         </Modal.Footer>
       </Modal>
     </>
-  );
+    )
+  }
 }
 
 export default App;
