847ee0edd90e26d8a320db8d71ecfba565f011e4
HP <ubuntu@ubuntu.ubuntu-domain>
2020-03-22 15:35:22 -0400

    added hashrouter with redirect. Removed this.state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, HashRouter, Link, Route,} from "react-router-dom";
+import {Switch, HashRouter, Route, Redirect} from "react-router-dom";
 import Login from "./components/login";
 import SignUp from "./components/signup";
 import './App.scss';
@@ -7,18 +7,18 @@ import './App.scss';
 class App extends React.Component {
   constructor(props){
     super(props);
-    this.state = {};
   }
 
   render(){
     return(
       <HashRouter>
         <Switch>
-          <Route path="/" render={Login}>
+          <Route path="/signup" component={SignUp}>
           </Route>
-          <Route path="/signup" render={SignUp}>
+          <Route path="/login" component={Login}>
           </Route>
-          <Route path="/login" render={Login}>
+          <Route path="/">
+            <Redirect to="/login"></Redirect>
           </Route>
         </Switch>
       </HashRouter>
