7bf5d96cfa57d059e7a8264fd2bd9deba7923bac
HP <ubuntu@ubuntu.ubuntu-domain>
2020-03-23 08:00:56 -0400

    forms

--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Form,Button} from 'react-bootstrap';
 import './login.scss';
 
 class Login extends React.Component {
@@ -9,7 +10,24 @@ class Login extends React.Component {
 
   render(){
     return(
-      <div className="credentials-bg"></div>
+      <div className="credentials-bg">
+        <Form className="bg-white p-3 custom-form rounded">
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Remember me" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Login
+          </Button>
+        </Form>
+      </div>
     )
   }
 }
--- frontend/src/components/signup/signup.jsx
+++ frontend/src/components/signup/signup.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Form, Button, Col } from 'react-bootstrap';
 import './signup.scss';
 
 class SignUp extends React.Component {
@@ -10,7 +11,29 @@ class SignUp extends React.Component {
   render(){
     return(
      <div className="credentials-bg">
-       
+       <Form className="bg-white p-3 custom-form rounded">
+       <Form.Row>
+          <Form.Group as={Col} controlId="formGridFirstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="text" placeholder="First Name" />
+          </Form.Group>
+          <Form.Group as={Col} controlId="formGridLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter last name" />
+          </Form.Group>
+        </Form.Row>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Sign Up
+        </Button>
+      </Form>
      </div>
     )
   }
