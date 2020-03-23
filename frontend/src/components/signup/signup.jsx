08caa7815a40d6fc2bac3c16199440f9c8b5faae
Sharan3009 <sharansingh3009@gmail.com>
2020-03-23 20:29:36 +0530

    added signup form

--- frontend/src/components/signup/signup.jsx
+++ frontend/src/components/signup/signup.jsx
@@ -15,7 +15,16 @@ class SignUp extends React.Component {
         firstName:"",
         email:"",
         password:"",
-        confirmPassword:""
+        confirmPassword:{
+          status:"",
+          text:""
+        }
+      },
+      formTouched:{
+        firstName:false,
+        email:false,
+        password:false,
+        confirmPassword:false
       },
       formValid:false
     }
@@ -26,6 +35,13 @@ class SignUp extends React.Component {
     this.setState({[name]:value},()=>this.validateField(name,value));
   }
 
+  setTouched = (event) => {
+    const {name,value} = event.target
+    let formTouched = this.state.formTouched;
+    formTouched[name] = true;
+    this.setState({formTouched},()=>this.validateField(name,value));
+  }
+
   submitForm = () =>{
     console.log(this.state)
   }
@@ -34,19 +50,35 @@ class SignUp extends React.Component {
     let fieldValidationErrors = this.state.formErrors;
     switch(fieldName) {
       case "firstName":
-      fieldValidationErrors.firstName = value ? '': 'First Name cannot be empty';
+        if(this.state.formTouched[fieldName]){
+          fieldValidationErrors.firstName = value ? '': 'First Name cannot be empty';
+        }
       break;
       case 'email':
-        let emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-        fieldValidationErrors.email = emailValid ? '' : 'Email is invalid';
+        if(this.state.formTouched[fieldName]){
+          let emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+          fieldValidationErrors.email = emailValid ? '' : 'Email is invalid';
+        }
         break;
       case 'password':
-        let passwordValid = value.length >= 6;
-        fieldValidationErrors.password = passwordValid ? '': 'Password is too short';
+        if(this.state.formTouched[fieldName]){
+          let passwordValid = value.length >= 6;
+          fieldValidationErrors.password = passwordValid ? '': 'Password is too short';
+        }
         break;
       case "confirmPassword":
-        let confirmPasswordValid = value === this.state.password;
-        fieldValidationErrors.password = confirmPasswordValid ? '': 'Passwords do not match';
+        if(value){
+          if(value===this.state.password){
+            fieldValidationErrors.confirmPassword.status = "valid";
+            fieldValidationErrors.confirmPassword.text = "Passwords Matched";
+          } else {
+            fieldValidationErrors.confirmPassword.status = "invalid";
+            fieldValidationErrors.confirmPassword.text = "Passwords do not match";
+          }
+        } else {
+          fieldValidationErrors.confirmPassword.status = "";
+          fieldValidationErrors.confirmPassword.text = "";
+        }
         break;
       default:
         break;
@@ -57,48 +89,67 @@ class SignUp extends React.Component {
   
   validateForm() {
     let formErrors = this.state.formErrors;
-    this.setState({formValid: !formErrors.firstName && !formErrors.email && !formErrors.password && !formErrors.confirmPassword});
+    this.setState({formValid: !formErrors.firstName && !formErrors.email && !formErrors.password && formErrors.confirmPassword.status==="valid"});
   }
 
   render(){
     return(
      <div className="credentials-bg">
-       <Form className="bg-white p-3 custom-form rounded" onSubmit={this.submitForm}>
+       <Form className="bg-white p-3 custom-form rounded" onSubmit={this.submitForm} method="POST">
        <Form.Row>
           <Form.Group as={Col} controlId="formGridFirstName">
             <Form.Label>First Name</Form.Label>
-            <Form.Control name="firstName" type="text" placeholder="First Name" value={this.state.firstName} onChange={this.setForm} isInvalid={this.state.formErrors.firstName} />
+            <Form.Control name="firstName" type="text" placeholder="First Name" 
+            value={this.state.firstName} 
+            onChange={this.setForm} 
+            isInvalid={this.state.formErrors.firstName}
+            onBlur={this.setTouched} />
             <Form.Control.Feedback type="invalid">
               {this.state.formErrors.firstName}
             </Form.Control.Feedback>
           </Form.Group>
           <Form.Group as={Col} controlId="formGridLastName">
             <Form.Label>Last Name</Form.Label>
-            <Form.Control name="lastName" type="text" placeholder="Enter last name" value={this.state.lastName} onChange={this.setForm} />
+            <Form.Control name="lastName" type="text" placeholder="Enter last name" 
+            value={this.state.lastName} 
+            onChange={this.setForm}
+            onBlur={this.setTouched} />
           </Form.Group>
         </Form.Row>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control name="email" type="email" placeholder="Enter email" value={this.state.email} onChange={this.setForm} isInvalid={this.state.formErrors.email}/>
+          <Form.Control name="email" type="email" placeholder="Enter email"
+          value={this.state.email}
+          onChange={this.setForm}
+          isInvalid={this.state.formErrors.email}
+          onBlur={this.setTouched}/>
           <Form.Control.Feedback type="invalid">
               {this.state.formErrors.email}
           </Form.Control.Feedback>
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control name="password" type="password" placeholder="Password" value={this.state.password} onChange={this.setForm} isInvalid={this.state.formErrors.password} />
+          <Form.Control name="password" type="password" placeholder="Password"
+          value={this.state.password}
+          onChange={this.setForm}
+          isInvalid={this.state.formErrors.password}
+          onBlur={this.setTouched} />
           <Form.Control.Feedback type="invalid">
               {this.state.formErrors.password}
           </Form.Control.Feedback>
         </Form.Group>
         <Form.Group controlId="formBasicConfirmPasswod">
           <Form.Label>Confirm Password</Form.Label>
-          <Form.Control name="confirmPassword" type="password" placeholder="Re-enter password" value={this.state.confirmPassword} onChange={this.setForm} isInvalid={this.state.formErrors.confirmPassword}/>
-          <Form.Control.Feedback type="invalid">
-              {this.state.formErrors.confirmPassword}
+          <Form.Control name="confirmPassword" type="password" placeholder="Re-enter password" 
+          value={this.state.confirmPassword} 
+          onChange={this.setForm} 
+          isInvalid={this.state.formErrors.confirmPassword.status==="invalid"}
+          isValid={this.state.formErrors.confirmPassword.status==="valid"}/>
+          <Form.Control.Feedback type={this.state.formErrors.confirmPassword.status}>
+              {this.state.formErrors.confirmPassword.text}
           </Form.Control.Feedback>
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" disabled={!this.state.formValid}>
           Sign Up
         </Button>
       </Form>
